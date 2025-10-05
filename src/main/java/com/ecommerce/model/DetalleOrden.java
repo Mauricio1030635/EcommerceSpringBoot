21c820d7dd293c5412f8182c36b1ae70ec1c7f5d
Mauricio1030635 <mauriciomedinasierra@gmail.com>
2025-10-05 11:55:54 -0500

    modelado de datos

--- src/main/java/com/ecommerce/model/DetalleOrden.java
+++ src/main/java/com/ecommerce/model/DetalleOrden.java
@@ -1,12 +1,37 @@
 package com.ecommerce.model;
 
+import java.util.Date;
+import java.util.List;
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "detalles")
 public class DetalleOrden {
-	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private double cantidad;
 	private double precio;
 	private double total;
+	
+	@ManyToOne
+	private Orden orden;
+	
+	@ManyToOne
+	private Producto producto;
+	
+	public DetalleOrden() {
+	
+	}
+	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.cantidad = cantidad;
+		this.precio = precio;
+		this.total = total;
+	}
 	public Integer getId() {
 		return id;
 	}
@@ -37,23 +62,23 @@ public class DetalleOrden {
 	public void setTotal(double total) {
 		this.total = total;
 	}
+	
+	
+	public Orden getOrden() {
+		return orden;
+	}
+	public void setOrden(Orden orden) {
+		this.orden = orden;
+	}
+	public Producto getProducto() {
+		return producto;
+	}
+	public void setProducto(Producto producto) {
+		this.producto = producto;
+	}
 	@Override
 	public String toString() {
 		return "DetalleOrden [id=" + id + ", nombre=" + nombre + ", cantidad=" + cantidad + ", precio=" + precio
 				+ ", total=" + total + "]";
 	}
-	public DetalleOrden(Integer id, String nombre, double cantidad, double precio, double total) {
-		super();
-		this.id = id;
-		this.nombre = nombre;
-		this.cantidad = cantidad;
-		this.precio = precio;
-		this.total = total;
-	}
-
-	public DetalleOrden() {
-		// TODO Auto-generated constructor stub
-	}
-	
-
 }
--- src/main/java/com/ecommerce/model/Producto.java
+++ src/main/java/com/ecommerce/model/Producto.java
@@ -1,16 +1,29 @@
 package com.ecommerce.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "productos")
 public class Producto {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nombre;
 	private String descripcion;
 	private String imagen;
 	private double precio;
-	private int cantidad; 
+	private int cantidad;
 	
+	@ManyToOne
+	private Usuario usuario;
 	
+	public Producto() {
+
+	}
+
 	
-	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+	public Producto(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad,
+			Usuario usuario) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
@@ -18,11 +31,10 @@ public class Producto {
 		this.imagen = imagen;
 		this.precio = precio;
 		this.cantidad = cantidad;
+		this.usuario = usuario;
 	}
 
-	public Producto() {
-		// TODO Auto-generated constructor stub
-	}
+
 
 	public Integer getId() {
 		return id;
@@ -71,13 +83,20 @@ public class Producto {
 	public void setCantidad(int cantidad) {
 		this.cantidad = cantidad;
 	}
+	
+	
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
 
 	@Override
 	public String toString() {
 		return "Producto [id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", imagen=" + imagen
 				+ ", precio=" + precio + ", cantidad=" + cantidad + "]";
 	}
-	
-	
-	
-}
+	}
